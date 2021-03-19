9c9a639dedeb2aabcc7a141c82cb153c0a6c3d79
min <minwang.bus@gmail.com>
2021-03-19 17:34:41 -0400

    working on phase 2 of two_sum

--- two_sum.rb
+++ two_sum.rb
@@ -12,10 +12,43 @@ end
 
 
 
-def okay_two_sum?(arr, target_sum)
+# def okay_two_sum?(arr, target_sum)
+#     # return false if arr.length < 2
+#     # return true if arr[0] + arr[1] == target_sum && arr.length == 2
+#     # pivot = arr[0]
 
-end
+#     # mid_ele = arr[mid]
 
-arr = [0, 1, 5, 7]
-p okay_two_sum?(arr, 6) # => should be true
-p okay_two_sum?(arr, 10) # => should be false
+#     # left = okay_two_sum?(arr[0...mid], target_sum)
+#     # right = okay_two_sum?(arr[mid..-1], target_sum)
+
+#     # left || right 
+
+#     return false if arr.length == 1 && arr[0] != target_sum 
+#     return true if arr[0] == target_sum
+
+#     mid = arr.length / 2
+        
+#     if  target_sum - arr[mid] < arr[mid]
+#         return okay_two_sum?(arr[0...mid], target_sum)
+#     else 
+#         return okay_two_sum?(arr[mid+1..-1], target_sum)
+#     end
+
+# end
+
+def two_sum?(arr, target_sum)
+    complements = {}
+  
+    arr.each do |el|
+        p el
+      return true if complements[target_sum - el]
+      complements[el] = true
+      p complements
+    end
+  
+    false
+  end
+arr = [0, 1, 3, 5, 7]
+p two_sum?(arr, 6) # => should be true
+# p two_sum?(arr, 10) # => should be false
